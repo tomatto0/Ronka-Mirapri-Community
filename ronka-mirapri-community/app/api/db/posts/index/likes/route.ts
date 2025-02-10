87d6c5bda576360340133c62ffd322c875528f33
toma <verumnium@gmail.com>
2025-02-10 14:08:19 +0900

    chore: api 주석수정

--- ronka-mirapri-community/app/api/db/posts/index/likes/route.ts
+++ ronka-mirapri-community/app/api/db/posts/index/likes/route.ts
@@ -3,6 +3,7 @@ import { connectDB, Like, Post } from "../../../database";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
+// 게시글 좋아요 GET요청
 export async function GET(request: Request) {
   try {
     const url = new URL(request.url);
@@ -14,6 +15,8 @@ export async function GET(request: Request) {
       );
     }
     await connectDB();
+
+    // 특정 index 값을 가진 게시물 조회 및 좋아요 개수 계산
     const post = await Post.aggregate([
       { $match: { index: index } },
       { $project: { _id: 1, like_count: { $size: "$likes" } } },
@@ -28,6 +31,8 @@ export async function GET(request: Request) {
 
     let is_liked = false;
     const session = await getServerSession(authOptions);
+
+    // 사용자가 로그인한 경우 해당 게시물에 좋아요를 눌렀는지 확인
     if (session?.user.login) {
       const like = await Like.findOne({
         post: post[0]._id,
