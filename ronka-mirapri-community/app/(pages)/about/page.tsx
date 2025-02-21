0ce06b8da2470dc51cac87a4bc30353e87ba479b
toma <verumnium@gmail.com>
2025-02-22 00:28:36 +0900

    styling: about-rulepage css

--- ronka-mirapri-community/app/(pages)/about/page.tsx
+++ ronka-mirapri-community/app/(pages)/about/page.tsx
@@ -5,34 +5,37 @@ import RulePage from "./components/RulePage";
 
 import { useState } from "react";
 
+import "../../css/About.css";
 export default function AboutPage() {
   const [category, set_category] = useState<string>("rule");
 
   return (
     <>
-      <div className="tlToggle">
-        <h3
-          className={category === "rule" ? "user-tap-active" : ""}
-          onClick={() => {
-            set_category("rule");
-          }}
-        >
-          운영 정책 및 이용 가이드
-        </h3>
-
-        <>
-          <div className="vertical-line" />
-          <h3
-            className={category === "rule" ? "" : "user-tap-active"}
+      <main className="about-wrap">
+        <div className="about-toggle">
+          <h5
+            className={category === "rule" ? "about-tap-active" : ""}
             onClick={() => {
-              set_category("introduce");
+              set_category("rule");
             }}
           >
-            사이트 소개
-          </h3>
-        </>
-      </div>
-      {category === "rule" ? <RulePage /> : <IntroducePage />}
+            운영정책 및 이용가이드
+          </h5>
+
+          <>
+            <div className="vertical-line" />
+            <h5
+              className={category === "rule" ? "" : "about-tap-active"}
+              onClick={() => {
+                set_category("introduce");
+              }}
+            >
+              사이트 소개
+            </h5>
+          </>
+        </div>
+        {category === "rule" ? <RulePage /> : <IntroducePage />}
+      </main>
     </>
   );
 }
