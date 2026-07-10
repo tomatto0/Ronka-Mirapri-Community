8defd33107d0d26309a313464828e90c1c483d4a
toma <verumnium@gmail.com>
2026-07-10 18:47:38 +0900

    Test: config설정 원복

--- ronka-mirapri-community/next.config.ts
+++ ronka-mirapri-community/next.config.ts
@@ -21,9 +21,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    optimizeCss: true,
-  },
+  // experimental: {
+  //   optimizeCss: true,
+  // },
 };
 
 export default nextConfig;
